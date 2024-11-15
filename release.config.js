1ddc5c0a4dc319424f471033d539117bd7830911
dora <31735614+dora1998@users.noreply.github.com>
2024-11-15 19:06:33 +0900

    feat: Initial Commit

---
+++ release.config.js
@@ -0,0 +1,38 @@
+/**
+ * @type {import('semantic-release').GlobalConfig}
+ */
+module.exports = {
+  branches: ["main", "0.+([0-9]).x"],
+  plugins: [
+    [
+      "@semantic-release/commit-analyzer",
+      {
+        preset: "angular",
+        releaseRules: [
+          { breaking: true, release: "major" },
+          { revert: true, release: "patch" },
+          { type: "feat", release: "minor" },
+          { type: "update", release: "minor" },
+          { type: "disable", release: "minor" },
+          { type: "!(release)", release: "patch" },
+        ],
+        parserOpts: {
+          revertPattern: /^(Revert|Reapply)\s"([\s\S]*)"$/m,
+          revertCorrespondence: ["header"],
+        },
+      },
+    ],
+    [
+      "@semantic-release/exec",
+      {
+        publishCmd: "echo ${nextRelease.version}",
+      },
+    ],
+    [
+      "@semantic-release/github",
+      {
+        successComment: false,
+      },
+    ],
+  ],
+};
